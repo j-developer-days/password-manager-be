337a902c4d4637f54b76d714fb66ab0a22632aca
j_dev <j.developer.days@gmail.com>
2023-05-26 15:39:45 +0300

    global settings of CORS

---
+++ service/src/main/java/com/jdev/passwordManager/config/RestConfig.java
@@ -0,0 +1,24 @@
+package com.jdev.passwordManager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4201/")
+                        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name());
+            }
+        };
+    }
+
+}
--- service/src/main/java/com/jdev/passwordManager/restController/GroupAccountRestController.java
+++ service/src/main/java/com/jdev/passwordManager/restController/GroupAccountRestController.java
@@ -37,7 +37,6 @@ public class GroupAccountRestController {
         }
     }
 
-    @CrossOrigin(origins = {"http://localhost:4201"})
     @GetMapping("/")
     public ResponseEntity<List<String>> getAllGroupAccount() {
         log.info("call get all group account endpoint");
